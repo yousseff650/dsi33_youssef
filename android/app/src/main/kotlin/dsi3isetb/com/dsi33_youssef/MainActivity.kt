0bc83020b0cf097aeb430a1d6975f00f3c187843
yousseff650 <yousseffarhat818@gmail.com>
2021-11-01 19:58:42 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/dsi3isetb/com/dsi33_youssef/MainActivity.kt
@@ -0,0 +1,6 @@
+package dsi3isetb.com.dsi33_youssef
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  MaterialApp(
+    home: Text('hello word'),
+  );
+}
